1899969d4c13bd3dfc05bf40bdf955147f151681
a-lakhani <56142442+a-lakhani@users.noreply.github.com>
2020-06-15 09:52:26 -0700

    Wrote memset and testing program

--- Circle_1/libft/srcs/mem/mem1.c
+++ Circle_1/libft/srcs/mem/mem1.c
@@ -6,8 +6,17 @@
 /*   By: alakhani <alakhani@student.42.us.org>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/05/27 18:12:19 by alakhani          #+#    #+#             */
-/*   Updated: 2020/05/27 18:12:21 by alakhani         ###   ########.fr       */
+/*   Updated: 2020/06/15 09:36:21 by alakhani         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+void *ft_memset(void *buffer, int ch, size_t num)
+{
+	unsigned char *c;
 
+	c = buffer;
+
+	while ( num-- )
+		*c++ = ch;
+	return buffer;
+}
---
+++ Circle_1/libft/srcs/testing.c
@@ -0,0 +1,42 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   testing.c                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: alakhani <alakhani@student.42.us.org>      +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2020/06/12 22:27:29 by alakhani          #+#    #+#             */
+/*   Updated: 2020/06/15 09:49:01 by alakhani         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <stdio.h>		// printf
+#include <stdlib.h>		// atoi
+#include <string.h>		// strcmp
+#include "mem/mem1.c"
+
+int	main(int argc, char *argv[])
+{
+	if ( argc > 1)
+	{
+		if ( !strcmp(argv[1],"ft_memset") && argc == 5)
+		{
+			int ch = atoi(argv[3]);
+			printf("String before memset: \"%s\"\n",argv[2]);
+			printf("Int given: %d\nInt cast as unsigned char: %c\n" 
+					"Integer result after & 255: %d\n"
+					"Int cast back from unsigned char: %d\n",
+					ch, (unsigned char) ch, ch & 255, (int) (unsigned char) ch);
+			printf("Writing %d times\n",atoi(argv[4]));
+			printf("String  after ft_memset: \"%s\"",
+			(char *) ft_memset((void *)argv[2],ch,atoi(argv[4])));
+		}
+		else if ( !strcmp(argv[1],"ft_memset") && argc != 5)
+			printf("Incorrect number of arguments given for ft_memset.\n");
+		else
+			printf("I don't know that function yet.\n");
+	}
+	else
+		printf("What did you want me to do?\n");
+	return (0);
+}
